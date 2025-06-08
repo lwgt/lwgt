83e131867ddf156b3e6b68df8f79d19460c4bbf8
lwgt <you@example.com>
2025-06-08 16:05:36 +0200

    Neues404errorneuerjsordner...jetztneueprojekteinjsordner

--- jslernern.js
+++
@@ -1,8 +0,0 @@
-function addContact() {
-    let nameInput = document.getElementById("nameInput");
-    let emailInput = document.getElementById("emailInput");
-    let numberInput = document.getElementById("numberInput");
-    document.getElementById("contactList").innerHTML += "<p style='display: flex; gap: 15px;'>" + nameInput.value + "</p>";
-    document.getElementById("contactList").innerHTML += "<p style='display: flex; gap: 15px;'>" + emailInput.value + "</p>";
-    document.getElementById("contactList").innerHTML += "<p style='display: flex; gap: 15px;'>" + numberInput.value + "</p>";
-}
