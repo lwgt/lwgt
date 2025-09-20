48a20d26810195aaa9c0cca1041a67c16cbecaf9
lwgt <you@example.com>
2025-09-20 10:09:55 +0200

    Cookie Clicker Error text du hast nicht genügend cookies

--- lwgt/Cookie Clicker/script.js
+++ lwgt/Cookie Clicker/script.js
@@ -3,8 +3,24 @@ let Points = 0
 let cookieText = "🍪Cookies🍪" + Points
 
 function Click() {
+    document.getElementById("Error").innerHTML = "";
     Points += plusCookies;
     console.log(Points)
     cookieText = "🍪Cookies🍪" + Points
     document.getElementById("currentCookies").innerHTML = cookieText;
+}
+
+
+function buy(Article) {
+    if (Article == "1") {
+        if (Points > 100) {
+            plusCookies += 1;
+            Points -= 100;
+        }
+        else {
+            document.getElementById("Error").innerHTML = "Du hast nicht genügend Cookies";
+        }
+    }
+    
+    
 }
