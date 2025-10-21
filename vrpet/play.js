089a853f6272c303d32dd86bc2cd2fff0ea57ed5
lwgt <you@example.com>
2025-10-21 17:51:15 +0200

    localstorage full fast fertig

---
+++ vrpet/play.js
@@ -0,0 +1,7 @@
+function petIsHappy() {
+    document.getElementById("feelings").innerHTML += "Dein Pet ist Glücklich!"
+}
+function petHasHunger() {
+    document.getElementById("feelings").innerHTML += "Dein Pet Hat Hunger. <br> Gebe ihm Futter!"
+}
+petIsHappy()
