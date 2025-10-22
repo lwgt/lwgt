8b46dabd543ecc813c03676645f9c5228dd86f5b
lwgt <you@example.com>
2025-10-22 16:09:25 +0200

    YES SIR!!!! ich hab bugs gefixt

--- vrpet/play.js
+++ vrpet/play.js
@@ -1,7 +1,17 @@
+if (localStorage.getItem("pet1") === null) {
+    localStorage.setItem("alreadyPlayed", null)
+    window.location.href='selectpet.html'
+}
+if (localStorage.getItem("alreadyPlayed") === null) {
+    window.location.href='selectpet.html'
+}
 function petIsHappy() {
     document.getElementById("feelings").innerHTML += "Dein Pet ist Glücklich!"
 }
 function petHasHunger() {
     document.getElementById("feelings").innerHTML += "Dein Pet Hat Hunger. <br> Gebe ihm Futter!"
+}
+function play() {
+    window.location.href='play.html'
 }
 petIsHappy()
