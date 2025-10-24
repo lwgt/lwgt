5817f2f262617402de469b11ae7be0e4f4318393
lwgt <you@example.com>
2025-10-24 16:44:06 +0200

    okok ganz viel neues!!!

--- js.js
+++ js.js
@@ -1,9 +1,3 @@
-function zufallszahl(min, max) {
+function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function warten(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-async function wait(miliseconds) {
-  await warten(miliseconds); // wartet 2 Sekunden
 }
--- vrpet/play.js
+++ vrpet/play.js
@@ -6,14 +6,34 @@ if (localStorage.getItem("alreadyPlayed") === null) {
     window.location.href='selectpet.html'
 }
 function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(randomNum / 60 / 1000)
+  return randomNum / 60
 }
 function petIsHappy() {
-    document.getElementById("feelings").innerHTML += "Dein Pet ist Glücklich!"
+    document.getElementById("feelings").innerHTML = "Dein Pet ist Glücklich!"
 }
 function petHasHunger() {
-    document.getElementById("feelings").innerHTML += "Dein Pet Hat Hunger. <br> Gebe ihm Futter!"
+    document.getElementById("feelings").innerHTML = "Dein Pet Hat Hunger. <br> Gebe ihm Futter!"
 }
+function moreResources(foodOfPetEN, foodOfPetDE) {
+    console.log("ahhhaaaa")
+    document.getElementById("feelings").innerHTML = "Du brauchst"
+}
+
+function feed() {
+        if (localStorage.getItem("pet1") === "Katze") {if (!(localStorage.getItem("mice")== "0")) {petIsHappy(); let mice = parseInt(localStorage.getItem("mice"));localStorage.setItem("mice", mice - 1)} else {moreResources("Mäuse")}}
+        else {
+            if (localStorage.getItem("pet1") === "Hase") {if (!(localStorage.getItem("carrots")== "0")) {petIsHappy(); let carrots = parseInt(localStorage.getItem("carrots"));localStorage.setItem("carrots", carrots - 1)} else {moreResources("Karotten")}}
+            else {
+                if (localStorage.getItem("pet1") === "Welpe") {if (!(localStorage.getItem("meat")== "0")) {petIsHappy(); let meat = parseInt(localStorage.getItem("meat"));localStorage.setItem("meat", meat - 1)}else {moreResources("Fleisch")}}
+                else {
+                    if (localStorage.getItem("pet1") === "Meerschweinchen") {console.log("ok"); if (!(localStorage.getItem("nuts")== "0")) {console.log("waaaassss?????"); petIsHappy(); let nuts = parseInt(localStorage.getItem("nuts"));localStorage.setItem("nuts", nuts - 1)}else {console.log("ok bis hier ghehts"); moreResources("Nüsse")}}
+                }
+            }
+        }
+}
+
 petIsHappy()
 // Seite wurde geladen
 console.log("Timer Startet")
@@ -21,4 +41,4 @@ console.log("Timer Startet")
 // Timer starten
 setTimeout(() => {
     petHasHunger()
-}, randomNumber(300000));
+}, randomNumber(300000, 150000));
